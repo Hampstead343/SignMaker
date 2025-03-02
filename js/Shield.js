3926e37d6acd27e78abb525d366c30f208f57828
Hampstead343 <hampstead343@hotmail.com>
2025-03-02 02:23:58 -0500

    Update Shield.js
    French language cardinal directions added.

--- js/Shield.js
+++ js/Shield.js
@@ -54,7 +54,12 @@ Shield.prototype.bannerTypes = [
 	"Toll",
 	"City",
 	"Conn",
-	"To"
+	"To",
+	"Est",
+	"Ouest",
+	"Sud",
+	"Nord",
+	"Via"
 ];
 Shield.prototype.bannerPositions = ["Above", "Right", "Left"];
 Shield.prototype.types = {
